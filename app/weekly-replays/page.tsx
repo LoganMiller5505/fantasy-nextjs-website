fb77958e30c5fa7e906686f21289243f71ce8be0
Logan Miller <dev@lmiller.io>
2026-09-25 09:59:53 -0500

    Shadcn-ui test

--- app/weekly-replays/page.tsx
+++ app/weekly-replays/page.tsx
@@ -1,10 +1,38 @@
-import Dropdown from "../_components/dropdown"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { Video } from "../_components/local_video";
+
+
+const items = [
+  { label: "Select a fruit", value: null },
+  { label: "Week 1", value: "1" },
+  { label: "Week 2", value: "2" },
+  { label: "Week 3", value: "3" }
+]
 
 export default function WeeklyReplaysPage() {
     return (
-        <div>
-            <Dropdown option={['Week 1', 'Week 2', 'Week 3']} defaultOption="Select Week" />
-        </div>
-    );
+    <Select items={items}>
+      <SelectTrigger className="w-full max-w-48">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          {items.map((item) => (
+            <SelectItem key={item.value} value={item.value}>
+              {item.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
 }
